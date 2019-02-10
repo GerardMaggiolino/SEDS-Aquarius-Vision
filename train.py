b6a21a633d71204532d20f4d5b78bc8d40d95ba0
Gerard Maggiolino <gerardmaggiolino@gmail.com>
2019-02-09 17:54:35 -0800

    Added messy z-scoring over training data. Not integrated with classify, will fix when entire dataset created.

--- train.py
+++ train.py
@@ -2,11 +2,14 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-from cnn import LanderCNN
+import cnn_v1
+import cnn_v2
 from dataloader import create_loaders
 from torchvision import transforms 
 from datetime import datetime
 
+from PIL import Image
+import time
 
 def main(): 
     ''' 
@@ -25,26 +28,36 @@ def train():
     epoch = 50
     batch = 16
     early_stop_ep = 8
-    split = [0.7, 0.2, 0.1]
+    split = [0.7, 0.1, 0.2]
     seed = None
     transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
         transforms.ToTensor()
     ])
 
+    # Data
+    train_set, val_set, test_set = create_loaders(split, transform, batch, seed)
+    mean, std = find_mean_std(train_set)
+    # Change dataset transform to z-score dynamically
+    print(f'Normalizing with mean: {mean}, std: {std}')
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[mean], std=[std])
+    ])
+    train_set.dataset.transform = transform
+
     # Model 
-    model = LanderCNN()
+    model = cnn_v1.LanderCNN()
     optim = torch.optim.Adam(model.parameters(), lr=alpha)
     crit = nn.BCEWithLogitsLoss()
 
     # Early stop 
     best_ep = 0
     t = datetime.now()
-    model_save_name = f'models/{t.month}{t.day}{t.hour}{t.minute}-model.pt'
+    model_save_name = (f'models/{model.__module__}'
+        f'{t.month}{t.day}{t.hour}{t.minute}.pt')
     info_file_name = 'models/model_info.txt'
-
-    # Data
-    train_set, val_set, test_set = create_loaders(split, transform, batch, seed)
 
     # Seed
     if seed is not None:
@@ -108,6 +121,7 @@ def train():
     # Save model information 
     with open(info_file_name, 'a') as f: 
         f.write(model_save_name)
+        f.write(f'Norm: Mean {mean} Std: {std}\n')
         f.write(f'\nC: {correct}\nT: {total}\n')
         f.write(f'{cat_acc}\n')
         f.write(f'{round(sum(correct) / sum(total) * 100, 3)}\n\n')
@@ -169,6 +183,33 @@ def test(data, model):
             correct[ind] == float('nan')
 
     return correct, total 
+
+def find_mean_std(data): 
+    '''
+    Returns mean, stddev for single channel data. 
+
+    Parameters
+    ----------
+    data : torch.utils.data.DataLoader
+
+    Returns
+    -------
+    float, float
+    mean, stddev across images.
+    '''
+    # Use Welford's method for running mean, stddev
+    mean = 0
+    var_num = 0 
+    n = 1
+    for batch_img, _ in data: 
+        for ind, k in enumerate(range(n, n + batch_img.shape[0]), 0):
+            x = batch_img[ind].mean()
+            old_mean = mean 
+            mean += (x - mean) / k
+            var_num += (x - mean) * (x - old_mean)
+        n += batch_img.shape[0]
+
+    return mean, np.sqrt(var_num / (n -2))
 
 
 if __name__ == '__main__': 
